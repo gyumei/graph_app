dfa80afb75318dd61cc70be79a7f9370da8d12b1
gyumei <hannibal221b219@gmail.com>
2023-11-29 14:25:05 +0900

    削除

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,14 +4,3 @@
  <meta charset="utf-8">
  <title>グラフ</title> 
  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-
-<!--自作CSS -->
-</head>
- <body>
-		<h1>グラフ</h1>
-		
-		{{ $inquiries->links() }}
-		
-
- </body>
-</html>
